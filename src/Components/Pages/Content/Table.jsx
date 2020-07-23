c4923c0c511e8444fbee6cbeb20783030b3292a7
Akshay <akshay28venkat@gmail.com>
2020-07-23 16:45:33 +0530

    Updated

--- src/Components/Pages/Content/Table.jsx
+++ src/Components/Pages/Content/Table.jsx
@@ -29,7 +29,7 @@ class Cretable extends React.Component{
                             .then(snapShot =>{
                               return snapShot.val()
                             })
-    
+    console.log(responseList)
     this.setState({responseList: responseList})
   }
 
